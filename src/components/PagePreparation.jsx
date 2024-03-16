a42a4a6eabc5f3af7c091638f69b3ea718c5ef52
zlarosav <zlarosav@gmail.com>
2024-03-15 23:58:15 -0500

    PagePreparation estilos terminados

--- src/components/PagePreparation.jsx
+++ src/components/PagePreparation.jsx
@@ -7,12 +7,18 @@ export default function PagePreparation() {
   const [amountMilkTea, setAmountMilkTea] = useState(3)
   const [amountBrownSugar, setAmountBrownSugar] = useState(2)
   const [amountBoba, setAmountBoba] = useState(2)
+  const [pricePerCup, setPricePerCup] = useState(12)
+
+  const costPerCup = amountMilkTea+(amountBrownSugar*2)+(amountBoba*2)
+  const profitPerCup = pricePerCup-costPerCup
+
+  const [stockSupplies, setStockSupplies] = useState(1)
 
   return (
     <section className="PagePreparation">
       <div className="cuerpo left-column">
 
-        <div className="left-column product-container">
+        <div className="product-container">
           <img src="./vaso.png" alt="Vaso" className="glass-img" />
           <div className="limiter" style={{ height: `${amountMilkTea * 9}%` }}>
             <img src="./milk_tea.png" alt="Leche con té" />
@@ -25,15 +31,30 @@ export default function PagePreparation() {
           </div>
         </div>
 
-        <div className="right-column">
+        <div className="quantifiers-container">
           <Quantifier minValue={amountBrownSugar + 1} inputValue={amountMilkTea} setInputValue={setAmountMilkTea}>Leche con té</Quantifier>
           <Quantifier minValue={2} maxValue={amountMilkTea - 1} inputValue={amountBrownSugar} setInputValue={setAmountBrownSugar}>Azúcar Morena</Quantifier>
           <Quantifier minValue={2} inputValue={amountBoba} setInputValue={setAmountBoba}>Boba</Quantifier>
+          <div className="cost-profit">
+            <p>Costo por copa: <span style={{fontWeight: 700}}>${costPerCup}</span></p>
+            {profitPerCup > 0 && <p>Ganancia: <span style={{fontWeight: 700}} className="green-word">$+{profitPerCup}</span></p>}
+            {profitPerCup <= 0 && <p>Ganancia: <span style={{fontWeight: 700}} className="red-word">${profitPerCup}</span></p>}
+          </div>
+          <Quantifier minValue={12} maxValue={50} priceValue={true} inputValue={pricePerCup} setInputValue={setPricePerCup}>Precio de Venta</Quantifier>
         </div>
       </div>
       <div className="right-column">
-        <div className="row-top"></div>
-        <div className="row-bottom"></div>
+        <div className="row-top">
+          <Quantifier minValue={1} maxValue={300} inputValue={stockSupplies} setInputValue={setStockSupplies} style={{width: "200px"}}>Reabastecer el stock <br/>de suministros</Quantifier>
+          <button className="btn-restock" disabled={true}>REABASTECER $0</button>
+          <div className="currentSupplies">
+            <p>Taza estimada de los<br/>suministros actuales</p>
+            <p>6</p>
+          </div>
+        </div>
+        <div className="row-bottom">
+          <button className="btn-startDay">Empezar día</button>
+        </div>
       </div>
     </section>
   )
--- src/components/Quantifier.jsx
+++ src/components/Quantifier.jsx
@@ -1,17 +1,21 @@
 import "../css/Quantifier.css"
 
-export default function PagePreparation({ minValue, maxValue, inputValue, setInputValue, children }) {
+export default function PagePreparation({ minValue, maxValue, priceValue, inputValue, setInputValue, style, children }) {
   return (
     <div className="quantifier">
-      <label htmlFor="input-milk-tea">{children}</label>
-      <input
-        type="number"
-        id="input-milk-tea"
-        min={minValue}
-        max={maxValue || 10}
-        value={inputValue}
-        onChange={e => setInputValue(Number(e.target.value))}
-      />
+      <label /* htmlFor={`input-${children.split(" ").join("-")}`} */>{children}</label>
+      <div className="input-wrapper">
+        {priceValue && <span>$</span>}
+        <input
+          type="number"
+          /* id={`input-${children.split(" ").join("-")}`} */
+          style={style}
+          min={minValue}
+          max={maxValue || 10}
+          value={inputValue}
+          onChange={e => setInputValue(Number(e.target.value))}
+        />
+      </div>
     </div>
   )
 }
